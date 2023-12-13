c73565ba01cdc4546e560a17c796699162a8c187
bodin <bodinsonsawan@gmail.com>
2023-12-13 16:54:19 +0700

    edit(ex7-8):answer adjustment

--- ex-6.js
+++ ex-6.js
@@ -1,9 +1,9 @@
 let height = undefined;
-let result = a(height);
+let result;
 
-function a(height) {
-  let result = height ?? "Height is not defined";
-  return result;
+function checkHeight(height) {
+  return height ?? "Height is not defined";
 }
 
+result = checkHeight(height);
 console.log(result);
--- ex-7.js
+++ ex-7.js
@@ -1,22 +1,19 @@
 const employee1 = {
   name: "John",
   age: 20,
-  scores: {
-    english: undefined,
-  },
 };
 
-const result1 = employee1.scores.english;
+const result1 = employee1.scores?.english; // เติม ? ในจุดที่ต้องการเช่น หลัง scores เพื่อใช้ optional chaining เพื่อให้ผลสามารถ return เป็น undefined และข้าม error
 console.log(result1);
+// ใส่ optional chaining เพื่อดัก bypass error ไว้เมื่อเราไม่ชัวร์ข้อมูล ว่า value ที่ได้มาอาจเป็น undefined
 
 const employee2 = {
   name: "A",
   age: 50,
   scores: {
     math: 40,
-    english: "English score is not defined",
   },
 };
 
-const result2 = employee2.scores.english;
+const result2 = employee2.scores.english ?? "English score is not defined";
 console.log(result2);
